c97426240ec7c011048cdf6ae4748f069148b299
sjw <yo21111@naver.com>
2022-02-22 17:17:11 +0900

    update aca_prac3

--- script/script.js
+++ script/script.js
@@ -1,18 +1,20 @@
 let notice = document.getElementById("bbsNotice");
 let gallery = document.getElementById("bbsGallery");
+let btnNotice = document.getElementById("button_notice");
+let bbsGallery = document.getElementById("button_gallery");
 
 document.getElementById("button_notice").addEventListener("click", fnNotice);
 function fnNotice() {
     notice.style.display="block";
     gallery.style.display="none";
-    document.getElementById("button_notice").className="activeTab";
-    document.getElementById("button_gallery").className="unactiveTab"
+    btnNotice.className="activeTab";
+    btnGallery.className="unactiveTab";
 }
 
 document.getElementById("button_gallery").addEventListener("click", fnGallery);
 function fnGallery() {
     notice.style.display="none";
     gallery.style.display="table";
-    document.getElementById("button_notice").className="unactiveTab";
-    document.getElementById("button_gallery").className="activeTab"
+    btnNotice.className="unactiveTab";
+    btnGallery.className="activeTab";
 }
